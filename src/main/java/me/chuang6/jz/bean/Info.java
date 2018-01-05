c263fde814c4794254034a3ddd19a338d235fdfa
liuchuang <409780903@qq.com>
2018-01-06 01:56:55 +0800

    彩票数据二级缓存

--- src/main/java/me/chuang6/jz/bean/Info.java
+++ src/main/java/me/chuang6/jz/bean/Info.java
@@ -1,45 +1,60 @@
 package me.chuang6.jz.bean;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Info {
-    private Integer id;
+public class Info implements Serializable {
+	private Integer id;
 
-    private Integer periods;
+	private Integer periods;
 
-    private String number;
+	private String number;
 
-    private Date addtime;
+	private Date addtime;
 
-    public Integer getId() {
-        return id;
-    }
+	public Info(int periods, String number) {
+		this.periods = periods;
+		this.number = number;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public Info() {
 
-    public Integer getPeriods() {
-        return periods;
-    }
+	}
 
-    public void setPeriods(Integer periods) {
-        this.periods = periods;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public String getNumber() {
-        return number;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setNumber(String number) {
-        this.number = number == null ? null : number.trim();
-    }
+	public Integer getPeriods() {
+		return periods;
+	}
 
-    public Date getAddtime() {
-        return addtime;
-    }
+	public void setPeriods(Integer periods) {
+		this.periods = periods;
+	}
 
-    public void setAddtime(Date addtime) {
-        this.addtime = addtime;
-    }
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number == null ? null : number.trim();
+	}
+
+	public Date getAddtime() {
+		return addtime;
+	}
+
+	public void setAddtime(Date addtime) {
+		this.addtime = addtime;
+	}
+
+	@Override
+	public String toString() {
+		return "期号:" + periods + " 号码:" + number + " 获取成功";
+	}
 }
