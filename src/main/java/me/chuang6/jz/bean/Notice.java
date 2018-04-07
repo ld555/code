a581f2dfef035605befe0d308a3882770ca9f53f
liuchuang <409780903@qq.com>
2018-04-07 17:22:33 +0800

    报警实体序列化

--- src/main/java/me/chuang6/jz/bean/Notice.java
+++ src/main/java/me/chuang6/jz/bean/Notice.java
@@ -1,8 +1,9 @@
 package me.chuang6.jz.bean;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Notice {
+public class Notice implements Serializable{
     private Integer id;
 
     private Integer periods;
